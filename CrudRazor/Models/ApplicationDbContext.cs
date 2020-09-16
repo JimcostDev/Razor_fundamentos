1b11cb494f87985358e6198333235d8bfdfc3d65
JimcostDev <rjak727@gmail.com>
2020-09-16 13:29:06 -0500

    Estableciendo conexión a bd (localdb) y configurando contexto de datos

---
+++ CrudRazor/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudRazor.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        //public DbSet<Course> Courses { get; set; }
+    }
+}
