8d181f7a67ac1b61fc52d9b0d9e20dd4c04dd1ef
JimcostDev <rjak727@gmail.com>
2020-09-28 17:01:28 -0500

    Formulario para crear curso

--- CrudRazor/Pages/Courses/Create.cshtml.cs
+++ CrudRazor/Pages/Courses/Create.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CrudRazor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,8 @@ namespace CrudRazor.Pages.Courses
 {
     public class CreateModel : PageModel
     {
+        [BindProperty]
+        public Course Course { get; set; }
         public void OnGet()
         {
         }
