3b5b86457d72a214f786e69d1f3bd54da0ccea69
JimcostDev <rjak727@gmail.com>
2020-09-29 16:54:16 -0500

    Mostrar registros, pagina index

--- CrudRazor/Pages/Courses/Index.cshtml.cs
+++ CrudRazor/Pages/Courses/Index.cshtml.cs
@@ -2,15 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CrudRazor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CrudRazor.Pages.Courses
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
         {
+            _db = db;
         }
+
+
+        public IEnumerable<Course> Courses { get; set; }
+
+        
+        public async Task OnGet()
+        {
+            Courses = await _db.Courses.ToListAsync();
+        }
+
     }
 }
