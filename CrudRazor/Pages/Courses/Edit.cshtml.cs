4fdcd6f09b0968d696f4390d2946a1c9e70790ef
JimcostDev <rjak727@gmail.com>
2020-10-01 15:35:27 -0500

    Formulario Editar registro y actualizar bd

---
+++ CrudRazor/Pages/Courses/Edit.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CrudRazor.Pages.Courses
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]// vincular con su respectiva vista
+        public Course Course { get; set; }
+        public async Task OnGet(int id)
+        {
+            Course = await _db.Courses.FindAsync(id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var CursoDesdeDb = await _db.Courses.FindAsync(Course.ID);
+                CursoDesdeDb.Name = Course.Name;
+                CursoDesdeDb.NumberClasses = Course.NumberClasses;
+                CursoDesdeDb.Price = Course.Price;
+
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
